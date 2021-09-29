0f85ffeb1f833d172cd3974ec5050f425e455d39
Philip Rehberger <me@philiprehberger.com>
2021-09-29 16:10:00 -0500

    Release v0.1.5

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+"""Basic import test."""
+
+
+def test_import():
+    """Verify the package can be imported."""
+    import philiprehberger_task_timer
+    assert hasattr(philiprehberger_task_timer, "__name__") or True
